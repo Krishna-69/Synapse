24cd04db9e7b92dc6ac6f964d5840c7b32d0e0f9
Krishna-69 <krishnarastogi67@gmail.com>
2026-06-16 10:16:41 +0530

    Added try/catch to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,22 +2,32 @@ import type { NextFunction, Request, Response } from "express";
 import jwt, { type JwtPayload } from "jsonwebtoken";
 import { JWT_PASSWORD } from "./config.js";
 
-
-export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const userMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
     const header = req.headers["authorization"];
-    const decoded = jwt.verify(header as string, JWT_PASSWORD)
-    if (decoded) {
-        if (typeof decoded === "string") {
-                return res.status(403).json({
-                message: "You are not logged in"
-            })
-        }
-        req.userId = (decoded as JwtPayload).id;
-        next();
-    } else {
-        return res.status(404).json({
-            message: "Incorect credentials"
-        })
-    }
 
-}
+    if (!header) {
+      return res.status(401).json({
+        message: "Authorization token missing",
+      });
+    }
+    const decoded = jwt.verify(header as string, JWT_PASSWORD);
+    if (decoded) {
+      if (typeof decoded === "string") {
+        return res.status(403).json({
+          message: "You are not logged in",
+        });
+      }
+      req.userId = (decoded as JwtPayload).id;
+      next();
+    }
+  } catch (e) {
+    return res.status(404).json({
+      message: "Incorect credentials",
+    });
+  }
+};
